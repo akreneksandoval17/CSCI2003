9b88e6ab5b614934b2a46d913d04b11801a21443
akreneksandoval17 <akreneksandoval@twu.edu>
2026-02-16 19:12:28 -0600

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,8 @@
+print ("input a number ^^")
+num = int(input())
+if num % 3 == 0 and num % 5 == 0:
+    print ("fizzbuzz")
+elif num % 3 == 0:
+    print ("fizz")
+elif num % 5 == 0:
+    print ("buzz")
